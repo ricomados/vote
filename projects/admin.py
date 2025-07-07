b2194bcb56cb2835968081cf237be139eab75310
김채은 <ricomados@gmail.com>
2025-07-08 02:59:28 +0900

    수정

--- projects/admin.py
+++ projects/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
+from django.db.models import Avg
 
 from .models import Project, Vote
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("title", "average_score", "created")
+    list_display = ("title", "average_score_display", "created")
     search_fields = ("title",)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(avg_score=Avg("votes__score")).order_by("-avg_score")
+
+    def average_score_display(self, obj):
+        """소수 1자리까지 표시하며, 정렬 가능 컬럼."""
+        return round(getattr(obj, "avg_score", 0) or 0, 1)
+
+    average_score_display.short_description = "평균 점수"
+    average_score_display.admin_order_field = "avg_score"
 
 
 @admin.register(Vote)
--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ProjectDetailView, ProjectListView, ResultView
+from .views import ProjectListView, ProjectDetailView, ResultView
 
 app_name = "projects"
 
